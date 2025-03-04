1b306adb4bc304130559a8b99cac562d0ecddf3f
cooleo273 <leulteferi273@gmail.com>
2025-03-04 09:51:02 +0300

    Add Header component and update layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
 
 const inter = Inter({subsets: ["latin"]});
 export const metadata: Metadata = {
@@ -16,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased`}
+        className={`${inter.className} antialiased bg-white min-h-[125vh]`}
+        cz-shortcut-listen="true"
       >
+        <Header />
         {children}
       </body>
     </html>
