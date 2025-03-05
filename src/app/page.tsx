527ab7c54a841210c010938efcc7e9ca0011862f
cooleo273 <leulteferi273@gmail.com>
2025-03-05 17:36:19 +0300

    Remove unused imports in Home component for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { getCurrentSession } from "@/actions/auth";
+
 import SalesCampaignBanner from "@/components/layout/SalesCampaignBanner";
 import ProductGrid from "@/components/product/ProductGrid";
 import { getAllProducts } from "@/sanity/lib/client";
-import { urlFor } from "@/sanity/lib/image";
-import Link from "next/link";
+
 
 const Home = async () => {
-  const { user } = await getCurrentSession();
+  
   const products = await getAllProducts();
 
   return (
