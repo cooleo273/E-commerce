956b68f84c4a017379f6253b6341d5acc546b7bc
cooleo273 <leulteferi273@gmail.com>
2025-03-05 17:39:10 +0300

    Refactor ProductGrid component by moving ProductGridProps type to a new types file for better organization

--- src/components/product/ProductGrid.tsx
+++ src/components/product/ProductGrid.tsx
@@ -1,11 +1,8 @@
-import { Product } from '@/sanity.types'
 import React from 'react'
 import ProductItem from './ProductItem'
+import { ProductGridProps } from './types'
 
-type ProductGridProps = {
-    products: Product[]
-  
-}
+
 const ProductGrid = ({products}:ProductGridProps) => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto container mt-4'>
