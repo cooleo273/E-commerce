05010a1f241e41dc92062666c7a00e2fdc72f344
cooleo273 <leulteferi273@gmail.com>
2025-03-05 08:01:49 +0300

    Add SignIn page with user authentication and form validation

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,43 @@
+import { getCurrentSession, loginUser, registerUser } from "@/actions/auth";
+import SignIn from "@/components/auth/SignIn";
+import { redirect } from "next/navigation";
+import React from "react";
+import zod from "zod";
+
+const SignUpSchema = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(6),
+});
+
+type FormState = { message: string };
+
+const SignInPage = async () => {
+  const { user } = await getCurrentSession();
+  if (user) {
+    return redirect("/");
+  }
+
+  const action = async (
+    prevState: FormState | undefined,
+    formData: FormData
+  ): Promise<{ message: string } | undefined> => {
+    "use server";
+    const parsed = SignUpSchema.safeParse(Object.fromEntries(formData));
+    if (!parsed.success) {
+      return { message: "Invalid form data" };
+    }
+    const { email, password } = parsed.data;
+    const { user, error } = await loginUser(email, password);
+    if (error) {
+      return { message: typeof error === "string" ? error : "Registration failed" };
+    } else if (user) {
+      
+      return redirect("/");
+      
+    }
+  };
+
+  return <SignIn action={action} />;
+};
+
+export default SignInPage;
